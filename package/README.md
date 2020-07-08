0748d6dbc2d40a3b928ce59fae54e90d00452f51
Idan Izicovich <idanizi@gmail.com>
2020-07-08 16:35:17 +0300

    package

--- package/README.md
+++
@@ -1,2 +0,0 @@
-# whatsapp-chrome-ext
-chrome extension for texting to WhatsApp without saving the number
--- package/manifest-creator.js
+++
@@ -1,9 +0,0 @@
-const fs = require('fs');
-const data = require('./manifest-data');
-
-fs.writeFile(
-    './manifest.json',
-    JSON.stringify(data),
-    error =>
-        error && console.log('cannot write manifest.json', { error })
-);
